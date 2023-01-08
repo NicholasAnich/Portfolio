2361fdae1dd4fd5e577f9fae486109c9fb360a77
Nicholas <infinitevolume@gmail.com>
2023-01-07 19:58:56 -0800

    Added content for about me page and styled contact form

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,52 +1,98 @@
-import { useState } from "react";
+import { useState, useRef } from 'react';
+import { useTheme } from 'next-themes';
+import clsx from 'clsx';
+import Popup from '../../components/popup/Popup.component';
+import styles from './contact.module.scss';
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [description, setDescription] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [showPopup, setShowPopup] = useState(false);
+  const { theme } = useTheme();
+  const contact = clsx(styles.container, styles[theme]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // submit the form
-    };
+  const form = useRef();
+  const [EMAIL_SERVICE_ID, EMAIL_TEMPLATE_ID, EMAIL_USER_ID] = [
+    'service_n9dms63',
+    'template_hnfdlno',
+    'FkedGJVRctISDpeR9',
+  ];
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">
-                Name:
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
-                />
-            </label>
-            <br />
-            <label htmlFor="email">
-                Email:
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                />
-            </label>
-            <br />
-            <label htmlFor="description">
-                Description:
-                <textarea
-                    id="description"
-                    name="description"
-                    value={description}
-                    onChange={(event) =>
-                        setDescription(event.target.value)
-                    }
-                />
-            </label>
-            <br />
-            <button type="submit">Submit</button>
-        </form>
-    );
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  const sendEmail = (event) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        EMAIL_SERVICE_ID,
+        EMAIL_TEMPLATE_ID,
+        form.current,
+        EMAIL_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          togglePopup();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div className={contact}>
+      {/* <input type='button' value='Click to Open Popup' onClick={togglePopup} /> */}
+      <p className={styles.contactMe}>
+        Let me know how I can help or just say Hi!
+      </p>
+      <form className={styles.form} ref={form} onSubmit={sendEmail}>
+        <label className={styles.label} htmlFor='name'>
+          Name:
+        </label>
+        <input
+          className={styles.input}
+          type='text'
+          id='name'
+          name='from_name'
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+        <label className={styles.label} htmlFor='email'>
+          Email:
+        </label>
+        <input
+          className={styles.input}
+          type='email'
+          id='email'
+          name='from_email'
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+        <label className={styles.label} htmlFor='message'>
+          Message:
+        </label>
+        <textarea
+          className={styles.textarea}
+          id='message'
+          name='message'
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          required
+        />
+        <input className={styles.submit} type='submit' value='Send' />
+      </form>
+      {showPopup && <Popup handleClose={togglePopup} />}
+    </div>
+  );
 }
