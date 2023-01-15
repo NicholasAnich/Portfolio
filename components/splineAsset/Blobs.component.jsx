1c05d6df6ae2291385c6bcd72b3347c1f4d695ec
Nicholas <infinitevolume@gmail.com>
2023-01-15 15:56:21 -0800

    added new spline scene and updated background color

--- components/splineAsset/Blobs.component.jsx
+++ components/splineAsset/Blobs.component.jsx
@@ -2,6 +2,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function Blobs() {
   return (
-    <Spline scene='https://prod.spline.design/mJY3D7j4MLlNh4iq/scene.splinecode' />
+    // <Spline scene='https://prod.spline.design/mJY3D7j4MLlNh4iq/scene.splinecode' />
+    // <Spline scene='https://prod.spline.design/grXm1mguPglsy1PA/scene.splinecode' />
+    <Spline scene='https://prod.spline.design/grXm1mguPglsy1PA/scene.splinecode' />
   );
 }
